920220bfb7337a1f85eba313db29637d61705b1e
Ashraf Hatem <78692509+Ashraf2500@users.noreply.github.com>
2024-09-25 00:37:49 +0300

    add local data to test ibad screeen

--- lib/presentation/bottom_bar_screens/views/modules_content/module_content.dart
+++ lib/presentation/bottom_bar_screens/views/modules_content/module_content.dart
@@ -57,10 +57,10 @@ class ModuleContentViews extends StatelessWidget {
               color: ColorManager.lightGrey,
               boxShadow: [
                 BoxShadow(
-                  color: ColorManager.lightGrey,
-                  offset:Offset(0,0.5),
-                  blurRadius: 2,
-                  spreadRadius: 1
+                    color: ColorManager.lightGrey,
+                    offset:Offset(0,0.5),
+                    blurRadius: 2,
+                    spreadRadius: 1
                 ),
               ],
             ),
@@ -71,92 +71,92 @@ class ModuleContentViews extends StatelessWidget {
                 ..add(GetSubjectsRequestEvent(moduleId:int.parse(module.id))),//todo module.id
               child: BlocBuilder<GetSubjectsBloc,GetSubjectsState>(
                   builder: (context , subjectsResponseState) {
-                  if(subjectsResponseState.requestState ==RequestState.loading){
-                    return Skeletonizer(
-                      child: ListView.separated(
-                        itemCount: 10,
+                    if(subjectsResponseState.requestState ==RequestState.loading){
+                      return Skeletonizer(
+                        child: ListView.separated(
+                          itemCount: 10,
+                          padding: EdgeInsets.symmetric(
+                            horizontal:AppPadding.pHScreen4(context),
+                            vertical:AppPadding.pVScreen2(context),
+                          ),
+                          separatorBuilder: (context, index) => Divider(color: ColorManager.lightGrey,thickness: 1,),
+                          itemBuilder: (context,index){
+                            return SubjectOfModuleItem(isMyLearning: isMyLearning,subject:SubjectEntity(id:"", image: "", imageUrl: "", name: "---------------------------", price: "----", doctors: [], chapters: [], description: "---------------------------"),);
+                          },
+                        ),
+                      );
+                    }else if(subjectsResponseState.requestState ==RequestState.done){
+                      //todo demo subject
+                      final List<SubjectEntity> subjectsDemo = [
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                        SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
+                      ];
+                      //return (subjectsResponseState.subjectsResponse.isNotEmpty)?ListView.separated(
+                      return (subjectsResponseState.subjectsResponse.isEmpty)?ListView.separated(
+                        //itemCount:subjectsResponseState.subjectsResponse.length,
+                        itemCount:subjectsDemo.length,
                         padding: EdgeInsets.symmetric(
                           horizontal:AppPadding.pHScreen4(context),
                           vertical:AppPadding.pVScreen2(context),
                         ),
                         separatorBuilder: (context, index) => Divider(color: ColorManager.lightGrey,thickness: 1,),
                         itemBuilder: (context,index){
-                          return SubjectOfModuleItem(isMyLearning: isMyLearning,subject:SubjectEntity(id:"", image: "", imageUrl: "", name: "---------------------------", price: "----", doctors: [], chapters: [], description: "---------------------------"),);
+                          //return SubjectOfModuleItem(isMyLearning: isMyLearning,subject: subjectsResponseState.subjectsResponse[index],);
+                          return SubjectOfModuleItem(isMyLearning: isMyLearning,subject: subjectsDemo[index],);
                         },
-                      ),
-                    );
-                  }else if(subjectsResponseState.requestState ==RequestState.done){
-                    //todo demo subject
-                    final List<SubjectEntity> subjectsDemo = [
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                      SubjectEntity(id: "6", image: AssetsManager.image_4, imageUrl: "https://repository-images.githubusercontent.com/195860046/06089740-270a-4cba-8b6e-2639377b5ccd", name: "Dart", price: "55", description: "description", doctors: [DoctorEntity(id: "12", image: AssetsManager.image_4, imageUrl: "", name: "moAbdulstar", phone: "01099516598", description: "Doctor at Kasr Alainy School of Medicine - Cairo University")], chapters: []),
-                    ];
-                    //return (subjectsResponseState.subjectsResponse.isNotEmpty)?ListView.separated(
-                    return (subjectsResponseState.subjectsResponse.isEmpty)?ListView.separated(
-                      //itemCount:subjectsResponseState.subjectsResponse.length,
-                      itemCount:subjectsDemo.length,
-                      padding: EdgeInsets.symmetric(
-                        horizontal:AppPadding.pHScreen4(context),
-                        vertical:AppPadding.pVScreen2(context),
-                      ),
-                      separatorBuilder: (context, index) => Divider(color: ColorManager.lightGrey,thickness: 1,),
-                      itemBuilder: (context,index){
-                        //return SubjectOfModuleItem(isMyLearning: isMyLearning,subject: subjectsResponseState.subjectsResponse[index],);
-                        return SubjectOfModuleItem(isMyLearning: isMyLearning,subject: subjectsDemo[index],);
-                      },
-                    ):CustomEmptyComponent(emptyItemType: "subject");
-                  }
-                  else{
-                    return GestureDetector(
-                      onTap: (){
-                        context.read<GetSubjectsBloc>().add(GetSubjectsRequestEvent(moduleId:int.parse(module.id)));
-                      },
-                      child: Container(
-                        width: AppConstants.wScreen(context),
-                        height: AppConstants.hScreen(context)*0.7,
-                        margin: EdgeInsets.symmetric(
-                          horizontal: AppPadding.pHScreen4(context),
-                          vertical: AppPadding.pVScreen2(context),
-                        ),
-                        decoration: BoxDecoration(
-                          color: ColorManager.lightGrey,
-                          borderRadius: BorderRadius.circular(AppRadius.r8),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              AssetsManager.warningImage,
-                              height: AppConstants.hScreen(context)*0.05,
-                              color: ColorManager.darkGrey.withOpacity(0.6),
-                            ),
-                            SizedBox(height: AppPadding.pVScreen1(context),),
-                            Text(
-                              subjectsResponseState.getSubjectsMessage,
-                              style: getMediumStyle(color: ColorManager.darkGrey),
-                              textAlign: TextAlign.center,
-                            ),
-                            SizedBox(height: AppPadding.pVScreen1(context),),
-                            Icon(
-                              Icons.refresh,
-                              color: ColorManager.darkGrey,
-                              size: AppSize.s20,
+                      ):CustomEmptyComponent(emptyItemType: "subject");
+                    }
+                    else{
+                      return GestureDetector(
+                        onTap: (){
+                          context.read<GetSubjectsBloc>().add(GetSubjectsRequestEvent(moduleId:int.parse(module.id)));
+                        },
+                        child: Container(
+                          width: AppConstants.wScreen(context),
+                          height: AppConstants.hScreen(context)*0.7,
+                          margin: EdgeInsets.symmetric(
+                            horizontal: AppPadding.pHScreen4(context),
+                            vertical: AppPadding.pVScreen2(context),
+                          ),
+                          decoration: BoxDecoration(
+                            color: ColorManager.lightGrey,
+                            borderRadius: BorderRadius.circular(AppRadius.r8),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                AssetsManager.warningImage,
+                                height: AppConstants.hScreen(context)*0.05,
+                                color: ColorManager.darkGrey.withOpacity(0.6),
+                              ),
+                              SizedBox(height: AppPadding.pVScreen1(context),),
+                              Text(
+                                subjectsResponseState.getSubjectsMessage,
+                                style: getMediumStyle(color: ColorManager.darkGrey),
+                                textAlign: TextAlign.center,
+                              ),
+                              SizedBox(height: AppPadding.pVScreen1(context),),
+                              Icon(
+                                Icons.refresh,
+                                color: ColorManager.darkGrey,
+                                size: AppSize.s20,
 
-                            ),
+                              ),
 
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    );
+                      );
+                    }
                   }
-                }
               ),
             ),
           ),
@@ -241,8 +241,8 @@ class SubjectOfModuleItem extends StatelessWidget {
               onTap: (){
                 Navigator.push(context, PageTransition(
                   child: SubjectContentViews(
-                      isMyLearning: isMyLearning,
-                      subject: subject,
+                    isMyLearning: isMyLearning,
+                    subject: subject,
                   ),
                   type: PageTransitionType.fade,
                   curve: Curves.fastEaseInToSlowEaseOut,
